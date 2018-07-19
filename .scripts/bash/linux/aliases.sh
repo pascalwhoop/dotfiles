503fa0a47aadc2b5ac71293414e8303aee6435d9
pascalwhoop <accounts@pascalbrokmeier.de>
2018-07-19 15:07:41 +0200

    clipmenu,style,rofi,dunst
    - switched from copyq to clipmenu
    - mpdris for music keys
    - styling gray transparent with polybar,urxvt,conky,dunst,rofi

--- .scripts/bash/linux/aliases.sh
+++ .scripts/bash/linux/aliases.sh
@@ -1,19 +1,18 @@
+###############################
 #alias and shortcuts
 ###############################
-#alias kub="kubectl"
-alias t="cd ~/Documents/Syncthing/Notes && vim ./todo.txt"
-alias n="cd ~/Documents/Syncthing/Notes/daily && vim ./`date +%Y-%m-%d-%A`.md"
+
+#note taking and todo list tricks
+###############################
+alias notedate="date +%Y-%m-%d-%A"
 alias isodate="date +%Y-%m-%d"
-alias rmswp='find ./ -name "*.swp" -exec rm {} \;'
-#alias jupystart="nvidia-docker start jupyter && docker exec -it jupyter bash -c \"jupyter notebook list\""
-#alias editconky="vim ~/.config/conky/pascalwhoop/conky.conf"
-#alias plexup="docker container start plex"
-#alias plexdown="docker container stop plex"
-#alias jnbup="docker container start tensorflow-notebook"
-alias goagent="cd ~/Documents/Code/University/powerTAC/python-agent && activate"
+alias t="cd ~/Documents/Syncthing/Notes && vim ./todo.txt"
+alias n="cd ~/Documents/Syncthing/Notes/daily && vim ./`notedate`.md"
+alias remcomm="echo \"!!\" >> ~/Documents/Syncthing/Notes/daily/`notedate`.md"
+
+
 alias makesmartpl="~/.config/mpd/generate_playlists.sh"
 #alias getss="xfce4-screenshooter -r -s ~/Desktop"
-alias pushagent="rsync --exclude="venv/" --exclude=".git/" --exclude=".idea/" --exclude="**/__pycache__"  --exclude=tensorboard --exclude=data -av /home/pascalwhoop/Documents/Code/University/powerTAC/python-agent/ pascalwhoop@25.33.211.37:/home/pascalwhoop/Documents/Code/University/powerTAC/python-agent"
 
 # cd to specific directories
 ###############################
@@ -46,6 +45,18 @@ alias open="xdg-open"
 ###############################
 alias activate="source venv/bin/activate"
 alias clearvim="rm -r /home/pascalwhoop/.vim/sessions\`pwd\`"
+alias rmswp='find ./ -name "*.swp" -exec rm {} \;'
+alias goagent="cd ~/Documents/Code/University/powerTAC/python-agent && activate"
+alias pushagent="rsync --exclude="venv/" --exclude=".git/" --exclude=".idea/" --exclude="**/__pycache__"  --exclude=tensorboard --exclude=data -av /home/pascalwhoop/Documents/Code/University/powerTAC/python-agent/ pascalwhoop@25.33.211.37:/home/pascalwhoop/Documents/Code/University/powerTAC/python-agent"
 
 #yaourt shortcuts
 alias y=yaourt
+
+# old / unused aliases
+###############################
+#alias jupystart="nvidia-docker start jupyter && docker exec -it jupyter bash -c \"jupyter notebook list\""
+#alias editconky="vim ~/.config/conky/pascalwhoop/conky.conf"
+#alias plexup="docker container start plex"
+#alias plexdown="docker container stop plex"
+#alias jnbup="docker container start tensorflow-notebook"
+#alias kub="kubectl"
--- .scripts/ipad_vert_conn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-xrandr --newmode "ipadvirtual"   54.75  1024 1072 1168 1312  1368 1371 1381 1395 +vsync
-xrandr --addmode VIRTUAL1 ipadvirtual
-xrandr --output VIRTUAL1 --right-of eDP1 --mode ipadvirtual
-x11vnc -clip 1024x1368+1920+0 -multiptr -passwd "myxps2016dp"
-xrandr --output VIRTUAL1 --off
-xrandr --delmode VIRTUAL1 ipadvirtual
-xrandr --rmmode ipadvirtual
