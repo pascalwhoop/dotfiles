557a4d0c3b87517e7ee84a89a0e0565d9c81901a
pascalwhoop <accounts@pascalbrokmeier.de>
2018-07-20 15:16:37 +0200

    switching to libinput with gestures

--- .config/conky/pascalwhoop/startConky.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sleep 5
-conky -c /home/pascalwhoop/.config/conky/pascalwhoop/conky.conf
--- .scripts/bash/linux.sh
+++ .scripts/bash/linux.sh
@@ -2,6 +2,7 @@ export EDITOR=vim
 export XDG_DATA_HOME="/home/pascalwhoop/.local/share"
 export XDG_CONFIG_HOME="/home/pascalwhoop/.config"
 export JAVA_HOME="/usr/lib/jvm/default"
+export CM_DIR="/home/pascalwhoop/Documents/Syncthing/Default Folder/copymenu"
 #setting display env variable for tmux
 export DISPLAY=":0"
 export QT_SCALE_FACTOR=1
