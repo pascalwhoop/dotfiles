e500471f5e5ebd65402b6b4f339b33eb146a63e3
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2018-11-22 16:22:21 +0100

    [FISH] added bass and omf

--- .config/fish/functions/screens.fish
+++ .config/fish/functions/screens.fish
@@ -1,7 +1,9 @@
 function screens
     if test $argv[1] = 'space'
         xrandr --output DP1-1 --auto --right-of eDP1
-        xrandr --output DP1-2 --auto --right-of DP1-1
+        xrandr --output DP1-2 --auto --right-of DP1-1 --rotate left
+        # restart my window manager
+        i3 restart
     end
     if test $argv[1] = 'off'
         xrandr --output DP1-1 --off
