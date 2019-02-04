98af93852cf6fe2e7bb747de952208432e980e37
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2019-02-04 19:53:37 +0100

    dp4k script

--- .config/fish/functions/screens.fish
+++ .config/fish/functions/screens.fish
@@ -9,10 +9,16 @@ function screens
         xrandr --output HDMI1 --auto --above eDP1
         i3 restart
     end
+    if test $argv[1] = 'dp4k'
+        xrandr --output DP1 --auto --pos 0x0
+        xrandr --output eDP1 --auto --pos 1008x2160
+        i3 restart
+    end
     if test $argv[1] = 'off'
         xrandr --output eDP1 --auto
         xrandr --output DP1-1 --off
         xrandr --output DP1-2 --off
         xrandr --output HDMI1 --off
+        xrandr --output DP1 --off
     end
 end
