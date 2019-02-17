36e999a8d3638725f3fa6ac2b50860eb1322f4c7
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2019-02-17 12:21:03 +0100

    switch to bass based env setting

--- .bash_profile
+++ .bash_profile
@@ -1,1 +1,15 @@
+#CUDA environment variables
+#setting display env variable for tmux
+export CM_DIR="/home/pascalwhoop/.config/clipmenu"
+export DISPLAY=":0"
+export DYNAMIC_COLORS_ROOT="/home/pascalwhoop/.config/dynamic-colors/"
+export EDITOR=/usr/bin/vim
+export JAVA_HOME="/usr/lib/jvm/java-10-openjdk"
+export XDG_CONFIG_HOME="/home/pascalwhoop/.config"
+export XDG_DATA_HOME="/home/pascalwhoop/.local/share"
+export PATH="/home/pascalwhoop/.gem/ruby/2.5.0/bin:$PATH"
+#export QT_SCALE_FACTOR=1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
+#my own ENV vars
+export NOTES="/home/pascalwhoop/Documents/Syncthing/Default Folder/Notebooks"
--- .config/fish/files/ENV_VARS.fish
+++ .config/fish/files/ENV_VARS.fish
@@ -1,18 +1,4 @@
-#CUDA environment variables
-#setting display env variable for tmux
-set -x CM_DIR "/home/pascalwhoop/.config/clipmenu"
-set -x DISPLAY ":0"
-set -x DYNAMIC_COLORS_ROOT "/home/pascalwhoop/.config/dynamic-colors/"
-set -x EDITOR /usr/bin/vim
-set -x JAVA_HOME "/usr/lib/jvm/java-10-openjdk"
-set -x QT_SCALE_FACTOR 1
-set -x XDG_CONFIG_HOME "/home/pascalwhoop/.config"
-set -x XDG_DATA_HOME "/home/pascalwhoop/.local/share"
-set -x PATH $PATH "/home/pascalwhoop/.gem/ruby/2.5.0/bin"
-
-#my own ENV vars
-set -x NOTES "/home/pascalwhoop/Documents/Syncthing/Default Folder/Notebooks"
-
+bass source ~/.bash_profile
 # omf bobthefish settings
 set -g theme_nerd_fonts yes
 set -g theme_newline_cursor yes
