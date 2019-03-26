13eaae857b7301aa56ee0a9ba449c45cfb4a0a7a
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2019-03-26 13:38:41 +0100

    adding fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/fish
 
+bass source ~/.config/locale.conf
+
 #sourcing functions / aliases / ...
 for file in (command ls ~/.config/fish/files)
     source ~/.config/fish/files/$file
@@ -13,3 +15,8 @@ end
 
 #virtualfish tooling
 eval (python -m virtualfish auto_activation)
+
+#enable fzf
+#https://wiki.archlinux.org/index.php/Fzf#fish
+fzf_key_bindings
+
