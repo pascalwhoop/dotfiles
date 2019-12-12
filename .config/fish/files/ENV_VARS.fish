bd3cbc1b1bf4685816ce1f0a65c03d9457507ed3
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2019-12-12 13:23:16 +0100

    scripts and font changed

--- .config/fish/files/ENV_VARS.fish
+++ .config/fish/files/ENV_VARS.fish
@@ -1,6 +1,11 @@
 bass source ~/.bash_profile
 # omf bobthefish settings
 set -g theme_nerd_fonts yes
+#set -g theme_powerline_fonts no
+set -g theme_display_date no
 set -g theme_newline_cursor yes
+set -g theme_command_duration no
+set -g theme_newline_prompt '$ '
+set -g theme_display_k8s_context no
 set -g theme_color_scheme solarized
 set -g VIRTUAL_ENV_DISABLE_PROMPT 1
--- .config/fish/functions/diary.fish
+++
@@ -1,8 +0,0 @@
-function diary
-    set FILE "$NOTES/diary/diary.gpg"
-    cat $FILE  | gpg --decrypt > /tmp/diary.md
-    vim /tmp/diary.md
-    cat /tmp/diary.md | gpg --encrypt -r mail@pascalbrokmeier.de > $FILE
-    rm -rf /tmp/diary.md
-end
-
