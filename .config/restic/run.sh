d773b3b3bc31d1f1b48f271ed19f288dc5b7baec
Pascal Brokmeier <git@pascalbrokmeier.de>
2021-03-07 14:03:45 +0100

    updates restic

--- .config/restic/run.sh
+++ .config/restic/run.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-sudo -E restic -r sftp:pi@192.168.1.99:/mnt/vault/Backup/mbp backup / --exclude-file=./excludes.txt $@
+sudo -E restic -vr backup sftp:pi:/mnt/vault/Backup/mbp --one-file-system --exclude-file=./excludes.txt $@
