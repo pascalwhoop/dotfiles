5972224abc3a50b2a2c6bf686aa04d5c75b9cb04
Pascal Brokmeier <git@pascalbrokmeier.de>
2021-03-09 00:26:57 +0100

    restic config

--- .config/restic/list.sh
+++ .config/restic/list.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 source .env
-sudo -E restic backup --one-file-system --exclude-file=./excludes.txt $@
+sudo -E restic snapshots
