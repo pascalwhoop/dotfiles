dfbd6af82b26c20d9eff43f1cbdbe68bf27db59e
Pascal Brokmeier <git@pascalbrokmeier.de>
2023-01-17 00:47:26 +0100

    adjusted bootstrap for debian

--- personal_scripts/.scripts/bootstrap/debian.sh
+++ personal_scripts/.scripts/bootstrap/debian.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+set -xe
 
 echo 'this script is something I use to bootstrap a new machine no matter where I work'
 
 # install core requirements
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/$USER/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew analytics off
 sudo apt install -y tmux htop bmon fzf wget curl
 
 # get brew working
+# ----------------
 git clone https://github.com/pascalwhoop/dotfiles
 cd dotfiles
 # stow all the things
+cat server.stows | xargs -I {} stow -t ~ {}
+
 
 
 # fish shell
