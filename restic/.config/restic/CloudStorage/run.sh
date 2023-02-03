bf20f3132c084a5ce09b215bd6b17e614a2bdefd
Pascal Brokmeier <git@pascalbrokmeier.de>
2023-02-03 12:48:22 +0100

    add restic

---
+++ restic/.config/restic/CloudStorage/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# check for root
+#!/bin/bash
+#if ! [ $(id -u) = 0 ]; then
+#   echo "Run as root!"
+#   exit 1
+#fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
+source .env
+restic backup --one-file-system --exclude-file=./excludes.txt /Users/pascal_brokmeier/
