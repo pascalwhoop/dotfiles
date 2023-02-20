3a75468e946fc6c99d81b103691d36c94289910b
Pascal Brokmeier <git@pascalbrokmeier.de>
2023-02-20 09:06:54 +0100

    add fix to restic

--- restic/.config/restic/CloudStorage/run.sh
+++ restic/.config/restic/CloudStorage/run.sh
@@ -10,4 +10,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 source ~/.config/restic_key/CloudStorage/.env
-restic backup --one-file-system --exclude-file=./excludes.txt /Users/pascal_brokmeier/
+restic backup --one-file-system --exclude-file=./excludes.txt /Users/pascalwhoop/
