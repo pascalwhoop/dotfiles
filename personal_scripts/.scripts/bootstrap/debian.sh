db4f90ef21021721946d15c81a938c26e0137587
Pascal Brokmeier <git@pascalbrokmeier.de>
2023-02-23 17:41:16 +0100

    updated debian bootstrap

--- personal_scripts/.scripts/bootstrap/debian.sh
+++ personal_scripts/.scripts/bootstrap/debian.sh
@@ -4,24 +4,26 @@ set -xe
 echo 'this script is something I use to bootstrap a new machine no matter where I work'
 
 # install core requirements
+sudo apt install -y tmux htop bmon fzf wget curl stow ranger
+
+# get brew working
+# ----------------
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/$USER/.profile
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew analytics off
-sudo apt install -y tmux htop bmon fzf wget curl stow ranger lsd
 
-# get brew working
-# ----------------
+# get dotfiles mounted
 git clone https://github.com/pascalwhoop/dotfiles
 cd dotfiles
 # stow all the things
-cat debian.stows | xargs -I {} stow -t ~ {}
+cat linux.stows | xargs -I {} stow -t ~ {}
 
 
 
 # fish shell
-brew install fish fzf bottom tldr exa dust duf fd jq dog gh terraform kubectl
+brew install fish fzf bottom tldr exa dust duf fd jq dog gh terraform kubectl k9s tig
 # oh my fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
