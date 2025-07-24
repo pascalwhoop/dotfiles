2e2481c352d93d7ca48718536fbe280abe1c2f87
Pascal Brokmeier <git@pascalbrokmeier.de>
2025-07-24 21:46:02 +0000

    crestic backup config

---
+++ rust/.cargo/env.fish
@@ -0,0 +1,5 @@
+# rustup shell setup
+if not contains "$HOME/.cargo/bin" $PATH
+    # Prepending path in case a system-installed rustc needs to be overridden
+    set -x PATH "$HOME/.cargo/bin" $PATH
+end
